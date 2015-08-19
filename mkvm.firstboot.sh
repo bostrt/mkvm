f2b79c3585987a13f4b84023b92afbe3c2435b4b
Robert Bost <rbost@redhat.com>
2015-08-19 16:28:11 -0400

    Initial commit. Code isn't in great state but mostly works.

---
+++ mkvm.firstboot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir /root/.ssh
+cat /root/remote_id_rsa.pub >> /root/.ssh/authorized_keys
+
+# password is 'redhat' ¬_¬
+useradd -r -s /bin/bash -m -p '$1$nzNcwr0G$ibOno/ZV60fAuy5lDkTjg.' redhat
+mkdir /home/redhat/.ssh
+touch /home/redhat/.ssh/authorized_keys
+cat /root/remote_id_rsa.pub >> /home/redhat/.ssh/authorized_keys
+chown -R redhat:redhat /home/redhat/.ssh
+
+rm /root/remote_id_rsa.pub
+
+# Expects a file 'rhn_creds' to exist. First line should have rhn username. Second line should have rhn password.
+RHN_USER=$(head -n 1 /root/rhn_creds)
+RHN_PASS=$(tail -n 1 /root/rhn_creds)
+
+subscription-manager --username=$RHN_USER --password=$RHN_PASS register --auto-attach
+
+rm /root/rhn_creds
