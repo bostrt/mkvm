7a804600dc867dbe6f311aba0bf67df791a2e74f
Coty Sutherland <csutherl@redhat.com>
2015-11-03 14:20:42 -0500

    Correcting some problems with the yumgroup type installation for puppet

--- plugins/puppet/init.sh
+++ plugins/puppet/init.sh
@@ -5,22 +5,35 @@
 if [ `grep '5\.' /etc/redhat-release | wc -l` -eq 1 ]; then
     rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-5.noarch.rpm;
     yum install puppet -y;
+
+    # copy the provider and type into place
+    mkdir /usr/lib/ruby/site_ruby/1.8/puppet/provider/yumgroup
+    # Need the absolute path here because it isn't executed from this directory
+    cp /root/plugins/puppet/custom-type/yumgroup/default.rb /usr/lib/ruby/site_ruby/1.8/puppet/provider/yumgroup/
+    cp /root/plugins/puppet/custom-type/yumgroup.rb /usr/lib/ruby/site_ruby/1.8/puppet/type/
 fi
 
 if [ `grep '6\.' /etc/redhat-release | wc -l` -eq 1 ]; then
     rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm;
     yum install puppet -y;
+
+    # copy the provider and type into place
+    mkdir /usr/lib/ruby/site_ruby/1.8/puppet/provider/yumgroup
+    # Need the absolute path here because it isn't executed from this directory
+    cp /root/plugins/puppet/custom-type/yumgroup/default.rb /usr/lib/ruby/site_ruby/1.8/puppet/provider/yumgroup/
+    cp /root/plugins/puppet/custom-type/yumgroup.rb /usr/lib/ruby/site_ruby/1.8/puppet/type/
 fi
 
 if [ `grep '7\.' /etc/redhat-release | wc -l` -eq 1 ]; then
     rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm;
     yum install puppet -y;
-fi
 
-# copy the provider and type into place
-mkdir /usr/lib/ruby/site_ruby/1.8/puppet/provider/yumgroup
-cp custom-type/yumgroup/default.rb /usr/lib/ruby/site_ruby/1.8/puppet/provider/yumgroup/
-cp custom-type/yumgroup.rb /usr/lib/ruby/site_ruby/1.8/puppet/type/
+    # copy the provider and type into place
+    mkdir /usr/share/ruby/vendor_ruby/puppet/provider/yumgroup
+    # Need the absolute path here because it isn't executed from this directory
+    cp /root/plugins/puppet/custom-type/yumgroup/default.rb /usr/share/ruby/vendor_ruby/puppet/provider/yumgroup/
+    cp /root/plugins/puppet/custom-type/yumgroup.rb /usr/share/ruby/vendor_ruby/puppet/type/
+fi
 
 # Included in the plugin are two manifests
 # Uncomment the line below to install the RPM version of the EWS/EAP product
