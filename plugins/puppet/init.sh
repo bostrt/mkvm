74332118368b24faf2bb6dc508686dd8ec298b27
Coty Sutherland <csutherl@redhat.com>
2015-11-03 13:48:21 -0500

    The operations were out of order. Puppet has to be installed before you can modify it :)

--- plugins/puppet/init.sh
+++ plugins/puppet/init.sh
@@ -1,11 +1,5 @@
 #!/bin/bash 
-# This script configures the required yumgroup type for installing yumgroups
-# and then confiure the puppet repo
-
-# copy the provider and type into place
-mkdir /usr/lib/ruby/site_ruby/1.8/puppet/provider/yumgroup
-cp custom-type/yumgroup/default.rb /usr/lib/ruby/site_ruby/1.8/puppet/provider/yumgroup/
-cp custom-type/yumgroup.rb /usr/lib/ruby/site_ruby/1.8/puppet/type/
+# This script installs puppet and then configures the required yumgroup type for installing yumgroups
 
 # Install the correct RHEL version of puppet
 if [ `grep '5\.' /etc/redhat-release | wc -l` -eq 1 ]; then
@@ -22,6 +16,11 @@ if [ `grep '7\.' /etc/redhat-release | wc -l` -eq 1 ]; then
     rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm;
     yum install puppet -y;
 fi
+
+# copy the provider and type into place
+mkdir /usr/lib/ruby/site_ruby/1.8/puppet/provider/yumgroup
+cp custom-type/yumgroup/default.rb /usr/lib/ruby/site_ruby/1.8/puppet/provider/yumgroup/
+cp custom-type/yumgroup.rb /usr/lib/ruby/site_ruby/1.8/puppet/type/
 
 # Included in the plugin are two manifests
 # Uncomment the line below to install the RPM version of the EWS/EAP product
