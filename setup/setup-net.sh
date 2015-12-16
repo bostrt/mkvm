236bcd7ea19da5252c0a85db185a351f3a509523
Coty Sutherland <csutherl@redhat.com>
2015-12-16 13:17:43 -0500

    Updating script to persist the default_static network

--- setup/setup-net.sh
+++ setup/setup-net.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Script to install and start the default_static network
 
-sudo virsh net-create --file default_static-net.xml
-sudo virsh net-start --network default_static
+sudo virsh net-define --file default_static-net.xml
+sudo virsh net-autostart --network default_static
